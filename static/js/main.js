9c1d5d914ab36f574b2187c0a81e0aebfc848acd
Joe Davis <davisj38@michigan.gov>
2020-02-25 14:34:49 -0500

    Progress on JS integration

--- static/js/main.js
+++ static/js/main.js
@@ -11,7 +11,7 @@ window.addEventListener('load', function() {
     // LoadMapboxMap();
 
     GetUrlParams();
-    MainVue.LoadMapboxMap();
+    // MainVue.LoadMapboxMap();
 
     // InitFilterFromTypeahead();
 
