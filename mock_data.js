2ea62947c4e3a531a0480aca7485558ea400ce95
Joe Davis <joedbns@gmail.com>
2020-09-16 09:08:17 -0400

    Add some mock data for facilities

---
+++ mock_data.js
@@ -0,0 +1,38 @@
+mock_data_facility = [
+    {
+        id: 456,
+        name: "Lansing Community College",
+        type: "IHL",
+        address: "Office of Veteran and Military Affairs, 333 N Washington Square",
+        city: "Lansing",
+        state: "MI",
+        zipcode: "48933",
+        latitude: "42.73693000000000",
+        longitude: "-84.55276000000000",
+        website: ""
+    },
+    {
+        id: 459,
+        name: "Program Managers Office - Light Armored Vehicles",
+        type: "OJT",
+        address: "6501 E. 11 MILE ROAD BLDG 229",
+        city: "WARREN",
+        state: "MI",
+        zipcode: "48397",
+        latitude: "42.50221500000000",
+        longitude: "-83.04046000000000",
+        website: ""
+    },
+    {
+        id: 461,
+        name: "International Trucking School",
+        type: "NCD",
+        address: "5840 Canton Center Rd Suite #270",
+        city: "CANTON",
+        state: "MI",
+        zipcode: "48187",
+        latitude: "42.32430900000000",
+        longitude: "-83.48637900000000",
+        website: ""
+    },
+]
